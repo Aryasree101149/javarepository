c99b124882b711f271920ff650af3b23c747ff64
Aryasree101149 <aryasree101149@gmail.com>
2026-02-27 14:19:49 +0530

    added 123

--- learnmaven/src/main/java/defaultaccesssample/Default_Sample.java
+++ learnmaven/src/main/java/defaultaccesssample/Default_Sample.java
@@ -2,7 +2,7 @@ package defaultaccesssample;
 
 public class Default_Sample {
  void sampledefault() { 
-	System.out.println("default limited to package");
+	System.out.println("default limited to package     123");
 }
 public static void main(String args[]) {
 	Default_Sample y = new Default_Sample();
