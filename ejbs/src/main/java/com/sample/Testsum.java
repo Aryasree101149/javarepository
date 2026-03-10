5b219dfdce27233131f0be610f07bd8c5c8b9e66
Aryasree101149 <aryasree101149@gmail.com>
2026-03-10 23:31:23 +0530

    commit

--- ejbs/src/main/java/com/sample/Testsum.java
+++ ejbs/src/main/java/com/sample/Testsum.java
@@ -2,10 +2,12 @@ package com.sample;
 
 public class Testsum {
 	static int a =10;
+	static int b=20;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(a);
+		int sum =a+b;
+System.out.println(sum);
 	}
 
 }
