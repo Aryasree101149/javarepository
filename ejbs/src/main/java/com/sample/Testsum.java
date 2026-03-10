e929baa5b57d0690e887b2ece6e02b09289a8d9a
Aryasree101149 <aryasree101149@gmail.com>
2026-03-10 23:28:28 +0530

    commit

---
+++ ejbs/src/main/java/com/sample/Testsum.java
@@ -0,0 +1,11 @@
+package com.sample;
+
+public class Testsum {
+	static int a =10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(a);
+	}
+
+}
